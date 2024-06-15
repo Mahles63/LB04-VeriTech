d5ca8f7c4d18444f28801b2d23a3cae8e55474cc
Mahlatse Lesley <57169692+Mahles63@users.noreply.github.com>
2024-06-15 16:07:23 +0200

    Add files via upload

---
+++ Task 4/script.js
@@ -0,0 +1,10 @@
+//settings carousel
+let navText =["<i class='bx bx-chevron-left'></i>,<i class='bx bx-chevron-right'></i>"]    
+$('#Cover-carousel').OwlCarousel2({
+items: 1,
+dots: false,
+loop: true,
+nav: true,
+navText: navText,
+autoplayHoverPause: true, 
+})
